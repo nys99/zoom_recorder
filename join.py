9abe659bc71f316d549e63d8e9561f8059ee845e
MrSwag <Nikhil Sharma>
2021-02-07 23:24:18 -0800

    initial commit

---
+++ join.py
@@ -0,0 +1,4 @@
+import os
+
+command = f'xdg-open "zoom://washington.zoom.us/join?confno=97793415868"'
+os.system(command)
---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+export SHELL=/bin/bash
+export SESSION_MANAGER=local/mrswag-Z390-I-AORUS-PRO-WIFI:@/tmp/.ICE-unix/1708,unix/mrswag-Z390-I-AORUS-PRO-WIFI:/tmp/.ICE-unix/1708
+export QT_ACCESSIBILITY=1
+export COLORTERM=truecolor
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
+export XDG_MENU_PREFIX=gnome-
+export GNOME_DESKTOP_SESSION_ID=this-is-deprecated
+export GNOME_SHELL_SESSION_MODE=ubuntu
+export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
+export XMODIFIERS=@im=ibus
+export DESKTOP_SESSION=ubuntu
+export SSH_AGENT_PID=1664
+export GTK_MODULES=gail:atk-bridge
+export PWD=/home/mrswag
+export LOGNAME=mrswag
+export XDG_SESSION_DESKTOP=ubuntu
+export XDG_SESSION_TYPE=x11
+export GPG_AGENT_INFO=/run/user/1000/gnupg/S.gpg-agent:0:1
+export XAUTHORITY=/run/user/1000/gdm/Xauthority
+export WINDOWPATH=2
+export HOME=/home/mrswag
+export USERNAME=mrswag
+export IM_CONFIG_PHASE=1
+export LANG=en_US.UTF-8
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
+export VTE_VERSION=6003
+export GNOME_TERMINAL_SCREEN=/org/gnome/Terminal/screen/483150d0_8983_4938_8036_985bbdd166fd
+export INVOCATION_ID=b984d81425f34ce18c77bf3b2a8d6251
+export MANAGERPID=1481
+export XDG_SESSION_CLASS=user
+export TERM=xterm-256color
+export USER=mrswag
+export GNOME_TERMINAL_SERVICE=:1.88
+export DISPLAY=:0
+export SHLVL=0
+export QT_IM_MODULE=ibus
+export XDG_RUNTIME_DIR=/run/user/1000
+# export PS1=\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ 
+export JOURNAL_STREAM=8:48692
+export XDG_DATA_DIRS=/usr/share/ubuntu:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop
+export PATH=/home/mrswag/.poetry/bin:/home/mrswag/.local/bin:/home/mrswag/.poetry/bin:/home/mrswag/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export GDMSESSION=ubuntu
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+export OLDPWD=/home/mrswag/Desktop/SideProjects/zoom_recordings
+export _=/usr/bin/printenv
+
+
+python3 /home/mrswag/Desktop/SideProjects/zoom_recordings/auto_zoom.py
+
